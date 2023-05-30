11899703b05cf731242597c9e189db172160cd3f
Sergey <volzhentsev@bk.ru>
2023-05-30 19:10:18 +0300

    fixed

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 function Toolbar(props) {
-  const {filters, onSelectFilter } = props
+  const {filters, selected, onSelectFilter } = props
   return (
     <div>
-      {filters.map((el, index) => {      
+      {filters.map((el, index) => { 
+        const className = el === selected ? 'filter-selected' : 'filter'     
         return (
-          <button onClick={(e) => onSelectFilter(el)} key={index}> {el} </button>   
+          <button onClick={(e) => onSelectFilter(el)} key={index} className={className}> {el} </button>   
         )
       })}
     </div>
